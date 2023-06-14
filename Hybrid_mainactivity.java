540fee370adaa983621ffe41a38fefdd9e845b8a
Keerthanaushar <120699074+Keerthanaushar@users.noreply.github.com>
2023-06-14 10:23:11 +0530

    Create Hybrid_mainactivity.java

---
+++ Hybrid_mainactivity.java
@@ -0,0 +1,26 @@
+package com.example.hybrid_based;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bPress;
+        bPress=(Button)findViewById(R.id.b1);
+        bPress.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"WELCOME",Toast.LENGTH_LONG).show();
+    }
+}
+
